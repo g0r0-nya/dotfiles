dfc8118ca21885129a9ea98c50c1b68f5ae4aa93
g0r0-nya <y_g0r0@yahoo.co.jp>
2017-06-15 00:38:36 +0900

    csvの読み書きの練習をした

--- py/test/csvfile.py
+++ py/test/csvfile.py
@@ -1,13 +1,19 @@
 #-*- coding:utf-8 -*-
 import csv
-rf = open('test.txt', 'r')
-wf = open('data.csv', 'wb')
-csvWriter = csv.writer(wf)
-txt_data = rf.read()
-rf.close()
-num_data = txt_data[:-1].split('\n')
 sum = 0
-for num in num_data:
-    sum += int(num)
-csvWriter.writerow(num_data)
+graph = [];
+wf = open('data.csv', 'wb')
+for i in ['00', '01']:
+    dir_path = i
+    for i in ['0']:
+        rf = open('/Users/56-n/py/test/' + dir_path + '/' + 'test' + str(i) +'.txt', 'r')
+        csvWriter = csv.writer(wf)
+        txt_data = rf.read()
+        rf.close()
+        num_data = txt_data[:-1].split('\n')
+        for num in num_data:
+            sum += int(num)
+    graph.append(sum)
+    sum = 0
+csvWriter.writerow(graph)
 wf.close()
